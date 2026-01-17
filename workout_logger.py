ec6f3141f1b3fb16969b22583671b1cc34061c12
Kameron Dam <kamxd@kmbp.home>
2026-01-17 02:32:33 -0500

    Updated with comments

--- workout_logger.py
+++ workout_logger.py
@@ -6,17 +6,37 @@
 workouts = []
 print("Workout Logger")
 
-#
-exercise = input("Exercise name: ")
+#Loop continues running until stopped with 'break'
+while True:
+# Ask use for exercise
+# User input, returns string
+    exercise = input("Exercise name (or 'done'): ")
+
+# If user types 'done', loop stops
+# == means "is equal to"
+
+    if exercise == "done":
+        break # exit loop
+
+# User input sets
 sets = int(input("Sets "))
+
+# User input reps
 reps = int(input("Reps: "))
 
+
+# Create dictionary to group related data
+# One dicitonary = one workout entry
 entry = {
     "exercise": exercise,
     "sets": sets,
     "reps": reps,
 }
 
+# Add dictionary to workout list
+# Append puts it at end of list
 workouts.append(entry)
+
+# Print all workouts after loop is done
 print(workouts)
 
